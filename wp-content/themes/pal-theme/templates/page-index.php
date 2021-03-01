23f8515c55205251000fa11430b0ac23c6e54f49
massagehero <onyx18121990@gmail.com>
2021-03-01 16:58:54 +0200

    add

--- wp-content/themes/pal-theme/templates/page-index.php
+++ wp-content/themes/pal-theme/templates/page-index.php
@@ -12,7 +12,7 @@ get_header(); ?>
         <img src="<?php echo IMG; ?>/mute.svg" alt="">
       </button> -->
 
-<div id="vidalytics_embed_PZJqPoMUfKd4SAI8" style="width: 100%; position:relative; padding-top: 56.25%;"></div>
+<div id="vidalytics_embed_<?php the_field('video'); ?>" style="width: 100%; position:relative; padding-top: 56.25%;"></div>
 <script type="text/javascript">
 (function (v, i, d, a, l, y, t, c, s) {
 y='_'+d.toLowerCase();c=d+'L';if(!v[d]){v[d]={};}if(!v[c]){v[c]={};}if(!v[y]){v[y]={};}var vl='Loader',vli=v[y][vl],vsl=v[c][vl + 'Script'],vlf=v[c][vl + 'Loaded'],ve='Embed';
@@ -22,7 +22,7 @@ if(s.readyState){s.onreadystatechange=function(){if(s.readyState==="loaded"||s.r
 i.getElementsByTagName("head")[0].appendChild(s);
 };}
 vsl(l+'loader.min.js',function(){if(!vli){var vlc=v[c][vl];vli=new vlc();}vli.loadScript(l+'player.min.js',function(){var vec=v[d][ve];t=new vec();t.run(a);});});
-})(window, document, 'Vidalytics', 'vidalytics_embed_PZJqPoMUfKd4SAI8', 'https://fast.vidalytics.com/embeds/isuMyTf1/PZJqPoMUfKd4SAI8/');
+})(window, document, 'Vidalytics', 'vidalytics_embed_<?php the_field('video'); ?>', 'https://fast.vidalytics.com/embeds/isuMyTf1/<?php the_field('video'); ?>/');
 </script>
 
     </div>
